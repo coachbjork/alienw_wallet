3f211e64bb3971903eab4df76f1c5a1718e9f39c
Lupo Solitario <duyluong1994@gmail.com>
2024-03-26 16:14:27 +0700

    enhance

--- src/routes/api/daoaw/msig_proposals/pending/[planet]/+server.ts
+++ src/routes/api/daoaw/msig_proposals/pending/[planet]/+server.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export async function GET({ params, request, cookies }) {
     const { planet } = params;
-    const response = await axios.get(`${ALIEN_WALLET_API}/proposals/${planet}?limit=100`);
+    const response = await axios.get(`${ALIEN_WALLET_API}/proposals/${planet}?limit=2`);
     if (response?.data) {
         let { data } = response;
         data = data.map((item: any) => {
