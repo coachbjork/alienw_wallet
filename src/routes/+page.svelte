0c8d5ca861e4a019d565ada8a5ce625a40d0a5bd
Lupo Solitario <duyluong1994@gmail.com>
2024-04-15 19:30:42 +0700

    goto voting as default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto('/voting');
+	});
+</script>
+
 <div class="main-content py-6">
 	<h1>Welcome to Alienw Wallet</h1>
 </div>
