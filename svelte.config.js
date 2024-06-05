5aa8c5d2db253cb843f450b7cdc64b246e7f9672
Lupo Solitario <duyluong1994@gmail.com>
2024-06-05 17:16:58 +0700

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/alienw_wallet' : '',
+			base: process.env.NODE_ENV === 'production' ? '/wallet_DAO_eosweden' : '',
 		}
 	},
 
